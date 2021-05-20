b80e11ddce4305b6ea6fb4ec2160612b46ee3edc
doringber123 <dori@pango.co.il>
2021-05-20 21:00:17 +0300

    first commit

---
+++ Automation.Core/Testing/TestCase.cs
@@ -0,0 +1,56 @@
+﻿using Automation.Core.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Automation.Core.Testing
+{
+    public abstract class TestCase
+    {
+        //fields
+        private IDictionary<string, object> testParams;
+        private int attempts;
+        private ILogger logger;
+
+        protected TestCase()
+        {
+            testParams = new Dictionary<string, object>();
+            attempts = 1;
+            logger = new TraceLogger();
+        } 
+
+        //components
+        public abstract bool AutomationTest(IDictionary<string, object> testParams);
+
+        public TestCase Execute()
+        {
+            Actual = AutomationTest(testParams);
+
+            return this;
+        }
+
+        //propertis
+        public bool Actual { get; set; }
+
+
+        //configuration
+        public TestCase WithTestParams(IDictionary<string, object> testParams)
+        {
+            this.testParams = testParams;
+            return this;
+        }
+
+        public TestCase WithNumberOfAttempts(int numberOfAttemptes)
+        {
+            attempts = numberOfAttemptes;
+            return this;
+        }
+
+        public TestCase WithLogger(ILogger logger)
+        {
+            this.logger = logger;
+            return this;
+        }
+
+    }
+}
---
+++ Automation.Extensions/Components/WebDriverFactory.cs
@@ -0,0 +1,99 @@
+﻿using Automation.Extensions.Contracts;
+using Newtonsoft.Json;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using System;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace Automation.Extensions.Components
+{
+    public class WebDriverFactory
+    {
+        private readonly DriverParams driverParams;
+
+        public WebDriverFactory(string driverParamsJson)
+            : this(LoadParams(driverParamsJson)) { }
+        
+
+        public WebDriverFactory(DriverParams driverParams)
+        {
+            this.driverParams = driverParams;
+            if (string.IsNullOrEmpty(driverParams.Binaries)||driverParams.Binaries == ".")
+            {
+                driverParams.Binaries = Environment.CurrentDirectory;
+            }
+        }
+
+        public string WebDriverManagerChrome()
+        {
+            driverParams.Binaries = new DriverManager().SetUpDriver(new ChromeConfig()).Replace("chromedriver.exe", "");
+            return driverParams.Binaries;
+            
+        }
+
+        public string WebDriverManagerFireFox()
+        {
+            driverParams.Binaries = new DriverManager().SetUpDriver(new FirefoxConfig()).Replace("geckodriver.exe", "");
+            return driverParams.Binaries;
+            
+        }
+
+        public IWebDriver Get()
+        {
+            if (!string.Equals(driverParams.Source, "REMOTE", StringComparison.OrdinalIgnoreCase))
+            {
+                return GetDriver();
+            }
+            return GertRemoteDriver();
+        }
+
+        //local webdriver
+        private IWebDriver GetChrome() => new ChromeDriver(WebDriverManagerChrome());
+
+        private IWebDriver GetFireFox() => new FirefoxDriver(WebDriverManagerFireFox());
+
+        private IWebDriver GetDriver()
+        {
+            switch (driverParams.Driver)
+            {
+                case "CHROME": return GetChrome();
+                case "FIREFOX": return GetFireFox();
+
+                default: return GertRemoteDriver();
+            }
+        }
+
+        //remote 
+        private IWebDriver GetRemoteChrome() => new RemoteWebDriver(new Uri(driverParams.Binaries), new ChromeOptions());
+
+        private IWebDriver GetRemoteFireFox() => new RemoteWebDriver(new Uri(driverParams.Binaries), new FirefoxOptions());
+
+
+
+
+        private IWebDriver GertRemoteDriver()
+        {
+            switch (driverParams.Driver)
+            {
+                case "CHROME": return GetRemoteChrome();
+                case "FIREFOX": return GetRemoteFireFox();
+
+                default: return GetChrome();
+            }
+        }
+
+
+        private static DriverParams LoadParams(string driverParamsJson)
+        {
+            if (string.IsNullOrEmpty(driverParamsJson))
+            {
+                return new DriverParams { Source = "Local", Driver = "Chrome", Binaries = "." };
+
+            }
+            return JsonConvert.DeserializeObject<DriverParams>(driverParamsJson);
+        }
+    }
+}
